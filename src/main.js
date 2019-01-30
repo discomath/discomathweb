673aff41823cab420520de65be173ecfa1f7a016
Vapurrmaid <vapurrmaid@gmail.com>
2019-01-30 00:44:18 -0500

    Move mtl-css from dependency to CDN

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import Router from 'vue-router'
 import routerConfig from './router'
 import storeConfig from './store/'
 
-import 'materialize-css/dist/css/materialize.css' // globally applied materialize css stylesheet
-
 import App from './App.vue'
 
 Vue.config.productionTip = false
