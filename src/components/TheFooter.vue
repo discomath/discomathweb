0780b59d7c9622e070292c0d0774a321654a8452
Vapurrmaid <vapurrmaid@gmail.com>
2019-01-30 00:38:44 -0500

    Update for gh deploy

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -15,7 +15,7 @@
         <div class="col l4 offset-l2 s12">
           <h5 class="white-text">Links</h5>
           <ul>
-            <li><a class="grey-text text-lighten-3" href="#!"><i class="inline-icon material-icons">code</i> Code</a></li>
+            <li><a class="grey-text text-lighten-3" href="https://github.com/discomath/discomathweb"><i class="inline-icon material-icons">code</i> Code</a></li>
           </ul>
         </div>
       </div>
